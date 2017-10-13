e88a18bb2a3242ef4188a70647a9a612fa2b724e
Markus Malkusch <markus@malkusch.de>
2017-10-13 21:01:54 +0200

    Implement BlockingConsumer's timeout
    Closes #9

--- classes/BlockingConsumer.php
+++ classes/BlockingConsumer.php
@@ -54,7 +54,11 @@ final class BlockingConsumer
      */
     public function consume($tokens)
     {
+        $timedOut = is_null($this->timeout) ? null : (microtime(true) + $this->timeout);
         while (!$this->bucket->consume($tokens, $seconds)) {
+            self::throwTimeoutIfExceeded($timedOut);
+            $seconds = self::keepSecondsWithinTimeout($seconds, $timedOut);
+            
             // avoid an overflow before converting $seconds into microseconds.
             if ($seconds > 1) {
                 // leave more than one second to avoid sleeping the minimum of one millisecond.
@@ -67,5 +71,38 @@ final class BlockingConsumer
             // sleep at least 1 millisecond.
             usleep(max(1000, $seconds * 1000000));
         }
+    }
+    
+    /**
+     * Checks if the timeout was exceeded.
+     *
+     * @param float|null $timedOut Timestamp when to time out.
+     * @throws TimeoutException The timeout was exceeded.
+     */
+    private static function throwTimeoutIfExceeded($timedOut)
+    {
+        if (is_null($timedOut)) {
+            return;
+        }
+        if (time() >= $timedOut) {
+            throw new TimeoutException("Timed out");
+        }
+    }
+    
+    /**
+     * Adjusts the wait seconds to be within the timeout.
+     *
+     * @param float $seconds Seconds to wait for the next consume try.
+     * @param float|null $timedOut Timestamp when to time out.
+     *
+     * @return float Seconds for waiting
+     */
+    private static function keepSecondsWithinTimeout($seconds, $timedOut)
+    {
+        if (is_null($timedOut)) {
+            return $seconds;
+        }
+        $remainingSeconds = max($timedOut - microtime(true), 0);
+        return min($remainingSeconds, $seconds);
     }
 }
